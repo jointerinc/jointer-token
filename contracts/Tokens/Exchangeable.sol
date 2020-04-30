72326100ef0d010a146a9ac27335760e459ebdca
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-04-30 20:33:49 +0530

    forceswap minting removed

--- contracts/Tokens/Exchangeable.sol
+++ contracts/Tokens/Exchangeable.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.5.9;
 
 import "./StandardToken.sol";
@@ -7,54 +6,47 @@ import "../InterFaces/ICurrencyPrices.sol";
 import "../InterFaces/ITokenVault.sol";
 import "../InterFaces/IToken.sol";
 
+
 contract ForceSwap is RestrictedToken {
-    
-    // here returnToken means with mainToken 
+    // here returnToken means with mainToken
     address public returnToken;
-    
-    constructor(address _returnToken) public notZeroAddress(_returnToken){
-        returnToken =_returnToken;
+
+    constructor(address _returnToken) public notZeroAddress(_returnToken) {
+        returnToken = _returnToken;
     }
 
- 
-    function forceSwap(address _which,uint256 _amount) external onlySystem() returns(bool){
-        
-        require(returnToken != address(0),"ERR_ACTION_NOT_ALLOWED");
-        
-        _burn(_which,_amount);
-        
+    function forceSwap(address _which, uint256 _amount)
+        external
+        onlySystem()
+        returns (bool)
+    {
+        require(returnToken != address(0), "ERR_ACTION_NOT_ALLOWED");
+
+        _burn(_which, _amount);
+
         ICurrencyPrices currencyPrice = ICurrencyPrices(getAddressOf(CURRENCY));
-        
+
         ITokenVault tokenVault = ITokenVault(getAddressOf(VAULT));
-        
+
         uint256 retunTokenPrice = currencyPrice.getCurrencyPrice(returnToken);
-        
-        uint256 currentTokenPrice = currencyPrice.getCurrencyPrice(address(this));
-        
-        uint256 _assignToken = safeDiv(safeMul(_amount,currentTokenPrice),retunTokenPrice);
-        
-        uint256 _vaultBalance = ERC20(returnToken).balanceOf(returnToken);
-         
-        if(_vaultBalance >= _assignToken){
-            tokenVault.directTransfer(returnToken,_which,_assignToken);
-        }else{
-            tokenVault.directTransfer(returnToken,_which,_vaultBalance);
-            _assignToken = safeSub(_assignToken,_vaultBalance);
-            IToken(returnToken).mintTokens(_assignToken);
-            ERC20(returnToken).transfer(_which,_assignToken);
-        }
-        
-        return true;
-        
+
+        uint256 currentTokenPrice = currencyPrice.getCurrencyPrice(
+            address(this)
+        );
+
+        uint256 _assignToken = safeDiv(
+            safeMul(_amount, currentTokenPrice),
+            retunTokenPrice
+        );
+
+        return tokenVault.directTransfer(returnToken, msg.sender, _assignToken);
     }
-    
 }
 
+
 contract Exchangeable is ForceSwap {
-    
-    address public exchangeableToken ; 
-    
-    
+    address public exchangeableToken;
+
     function setExchangeableToken(address _which)
         external
         onlySystem()
@@ -63,9 +55,12 @@ contract Exchangeable is ForceSwap {
         require(exchangeableToken == address(0), "ERR_ACTION_NOT_ALLOWED");
         exchangeableToken = _which;
     }
-    
+
     modifier isConversionAllowed(address _which) {
-        require(_which == exchangeableToken || _which == returnToken , "ERR_TOKEN_IS_NOT_IN_LIST");
+        require(
+            _which == exchangeableToken || _which == returnToken,
+            "ERR_TOKEN_IS_NOT_IN_LIST"
+        );
         _;
     }
 
@@ -74,28 +69,27 @@ contract Exchangeable is ForceSwap {
         isConversionAllowed(_fromToken)
         returns (uint256)
     {
-        
         ICurrencyPrices currencyPrice = ICurrencyPrices(getAddressOf(CURRENCY));
-        
+
         uint256 fromTokenPrice = currencyPrice.getCurrencyPrice(_fromToken);
-        
+
         uint256 currentTokenPrice = currencyPrice.getCurrencyPrice(
             address(this)
         );
-        
+
         uint256 _assignToken = safeDiv(
             safeMul(_amount, fromTokenPrice),
             currentTokenPrice
         );
-        
+
         ERC20(_fromToken).transferFrom(
             msg.sender,
             getAddressOf(VAULT),
             _amount
         );
-        
+
         _mint(msg.sender, _assignToken);
-        
+
         return _assignToken;
     }
 
@@ -105,11 +99,11 @@ contract Exchangeable is ForceSwap {
         returns (bool)
     {
         ICurrencyPrices currencyPrice = ICurrencyPrices(getAddressOf(CURRENCY));
-        
+
         ITokenVault tokenVault = ITokenVault(getAddressOf(VAULT));
-        
+
         uint256 toTokenPrice = currencyPrice.getCurrencyPrice(_toToken);
-        
+
         uint256 currentTokenPrice = currencyPrice.getCurrencyPrice(
             address(this)
         );
@@ -117,16 +111,10 @@ contract Exchangeable is ForceSwap {
             safeMul(_amount, currentTokenPrice),
             toTokenPrice
         );
-    
-        tokenVault.directTransfer(_toToken,msg.sender,_assignToken);
+
+        tokenVault.directTransfer(_toToken, msg.sender, _assignToken);
         _burn(msg.sender, _amount);
-        
+
         return true;
-    
     }
-
 }
-
-
-
-
