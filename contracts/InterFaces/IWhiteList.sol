520a8350044546ef60895b30c5a37afe120be00b
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-05-18 19:03:02 +0530

    change in token,whitelist , Iwhitelist

--- contracts/InterFaces/IWhiteList.sol
+++ contracts/InterFaces/IWhiteList.sol
@@ -1,17 +1,22 @@
 pragma solidity ^0.5.9;
 
+
 contract IWhiteList {
-    
     function isWhiteListed(address _who) public view returns (bool);
 
-    function checkBeforeTransfer(address _from, address _to)
+    function isAddressByPassed(address _which) public view returns (bool);
+
+    function hasPermission(address _from, address _to)
         public
         view
         returns (bool);
 
-    function isAddressByPassed(address _which) public view returns (bool);
-    
-    function canBuyToken(address _token,address _which) public view returns (bool);
-    
-    
+    function canReciveToken(address _which) public view returns (bool);
+
+    function isRestrictedAddress(address _which) public view returns (bool);
+
+    function isTransferAllowed(address _from, address _to)
+        public
+        view
+        returns (bool);
 }
--- contracts/Tokens/RestrictedToken.sol
+++ contracts/Tokens/RestrictedToken.sol
@@ -1,34 +1,49 @@
 pragma solidity ^0.5.9;
 
-
 import "./TokenUtils.sol";
 import "../InterFaces/IWhiteList.sol";
 
 
 contract RestrictedToken is TokenUtils {
-
-    
     function checkBeforeTransfer(address _from, address _to)
         internal
         view
         returns (bool)
     {
-        
         address whiteListAddress = getAddressOf(WHITE_LIST);
-        
-        if (IWhiteList(whiteListAddress).isAddressByPassed(msg.sender) == false) {
-            
-            require(IWhiteList(whiteListAddress).checkBeforeTransfer(_from, _to),"ERR_TRANSFER_CHECK_WHITELIST");
-            
-            require(!isTokenMature() && isHoldbackDaysOver(),"ERR_ACTION_NOT_ALLOWED");
+
+        require(
+            IWhiteList(whiteListAddress).isWhiteListed(_to),
+            "ERR_USER_NOT_WHITELISTED"
+        );
+
+        if (IWhiteList(whiteListAddress).isRestrictedAddress(_to)) {
+            require(
+                IWhiteList(whiteListAddress).hasPermission(_from, _to),
+                "ERR_NOT_HAVE_PERMISSION_TO_TRANSFER"
+            );
+        } else if (
+            IWhiteList(whiteListAddress).isAddressByPassed(msg.sender) == false
+        ) {
+            require(
+                IWhiteList(whiteListAddress).isTransferAllowed(_from, _to),
+                "ERR_NOT_HAVE_PERMISSION_TO_TRANSFER"
+            );
+            require(
+                IWhiteList(whiteListAddress).canReciveToken(_to),
+                "ERR_NOT_KYC_AML_EXPIRED"
+            );
+            require(
+                !isTokenMature() && isHoldbackDaysOver(),
+                "ERR_ACTION_NOT_ALLOWED"
+            );
         }
         return true;
-    
     }
-    
+
     function transfer(address _to, uint256 _value) external returns (bool ok) {
         require(checkBeforeTransfer(msg.sender, _to));
-        return _transfer(msg.sender,_to, _value);
+        return _transfer(msg.sender, _to, _value);
     }
 
     function transferFrom(address _from, address _to, uint256 _value)
@@ -38,7 +53,4 @@ contract RestrictedToken is TokenUtils {
         require(checkBeforeTransfer(_from, _to));
         return _transferFrom(_from, _to, _value);
     }
-    
-    
-    
 }
