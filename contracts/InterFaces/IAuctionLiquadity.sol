54e27b0f2c9f9508da02c9b03e569e1c4347a073
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-05-19 11:46:30 +0530

    jntr price from price contarct

--- contracts/InterFaces/IAuctionLiquadity.sol
+++ contracts/InterFaces/IAuctionLiquadity.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.5.9;
-import './IERC20Token.sol';
+import "./IERC20Token.sol";
+
 
 contract IAuctionLiquadity {
-    function getCurrentMarketPrice() public view returns(uint256);
-    
     function contributeWithEther() public payable returns (uint256);
-    
+
     function auctionEnded(uint256 auctionDayId) public returns (bool);
-    
+
     function contributeWithToken(
         IERC20Token _token,
         address _from,
