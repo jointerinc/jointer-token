2c8d6d3809a243d0cf27c048fc19738031743912
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-05-26 10:56:01 +0530

    change in liquadity,auction formula

--- contracts/InterFaces/IAuctionFormula.sol
+++ contracts/InterFaces/IAuctionFormula.sol
@@ -2,34 +2,18 @@ pragma solidity ^0.5.9;
 
 
 contract IAuctionFormula {
-    function calculateLiquadityMainReserve(
-        uint256 yesterdayPrice,
-        uint256 dayBeforyesterdayPrice,
-        uint256 yesterDaycontibution,
-        uint256 yesterdayMainReserv
-    ) external pure returns (uint256);
-
-    function calculateTokenPrice(
-        uint256 _reserveBaseTokenBalance,
-        uint256 _reserveBaseTokenRatio,
-        uint256 _reserveMainTokenBalance,
-        uint256 _reserveMainTokenRatio,
-        uint256 _baseTokenPrice
-    ) public pure returns (uint256);
-
-    function calculateRecoverPrice(
-        uint256 _reserveTokenBalance,
-        uint256 _reserveTokenRatio,
-        uint256 _needPrice,
-        uint256 _reserveFindTokenRatio,
-        uint256 _TokenPrice
-    ) external pure returns (uint256);
-
     function calcuateAuctionFundDistrubution(
         uint256 _value,
         uint256 downSideProtectionRatio,
         uint256 fundWalletRatio
-    ) public pure returns (uint256, uint256, uint256);
+    )
+        public
+        pure
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
 
     function calculateMangmentFee(uint256 _supply, uint256 _percent)
         external
