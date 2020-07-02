79a04e8b2e6b7c9108165cab5dfa6990ac7fd24f
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-07-02 18:23:47 +0530

    Auction change and Whitelist change

--- contracts/InterFaces/IIndividualBonus.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.9;
-
-
-interface IIndividualBonus {
-    function calucalteBonus(uint256 _userIndex, uint256 returnAmount)
-        external
-        view
-        returns (uint256);
-}
--- contracts/WhiteList/WhiteList.sol
+++ contracts/WhiteList/WhiteList.sol
@@ -20,13 +20,13 @@ interface InitializeInterface {
     ) external;
 }
 
-
 contract WhiteListStorage {
     struct UserDetails {
         uint256 flags; // bitmap, where each bit correspondent to some properties (additional flags can be added in future).
         uint256 maxWallets;
         address[] wallets;
     }
+
     // Whitelist rules
     struct ReceivingRule {
         uint256 mask; // bit-mask to select bits which will be checked by this rule
@@ -55,6 +55,11 @@ contract WhiteListStorage {
     mapping(uint8 => ReceivingRule) public tokenToReceivingRule;
     mapping(uint8 => TransferringRule[]) public tokenToTransferringRuleArray;
 
+    //timestamp when hold back days are over
+    mapping(uint8 => uint256) public tokenToHoldBackDaysTimeStamp;
+    //timestamp when token matures
+    mapping(uint8 => uint256) public tokenToMaturityDaysTimeStamp;
+
     uint256 public constant KYC = 1 << 0; //0x01
     uint256 public constant AML = 1 << 1; //0x02
     uint256 public constant ACCREDIATED_INVESTOR = 1 << 2;
@@ -70,14 +75,16 @@ contract WhiteListStorage {
     uint256 public constant FROM_EU = 1 << 12;
     uint256 public constant IS_BYPASSED = 1 << 13;
     uint256 public constant BANCOR_ADDRESS = 1 << 14;
-    //timestamp when hold back days are over
-    mapping(uint8 => uint256) public tokenToHoldBackDaysTimeStamp;
-    //timestamp when token matures
-    mapping(uint8 => uint256) public tokenToMaturityDaysTimeStamp;
+    uint256 public constant IS_ALLOWED_AUCTION = 1 << 15;
 }
 
-
-contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,InitializeInterface {
+contract WhiteList is
+    Upgradeable,
+    ProxyOwnable,
+    SafeMath,
+    WhiteListStorage,
+    InitializeInterface
+{
     event AccountWhiteListed(address indexed which, uint256 flags);
     event WalletAdded(address indexed from, address indexed which);
     event WalletRemoved(address indexed from, address indexed which);
@@ -108,12 +115,8 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         uint256 _stockMaturityDays
     ) public {
         super.initialize();
-        
-        initializeOwner(
-            _primaryOwner,
-            _systemAddress,
-            _authorityAddress
-        );
+
+        initializeOwner(_primaryOwner, _systemAddress, _authorityAddress);
         tokenToMaturityDaysTimeStamp[0] = convertDaysToTimeStamp(
             _mainMaturityDays
         );
@@ -149,24 +152,59 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         return true;
     }
 
-    /**@dev returns if address is whitelisted*/
-    function isWhiteListed(address _which) public view returns (bool) {
-        if (address_belongs[_which] == address(0)) return false;
-        return true;
+    /**@dev checks if address is bancor's*/
+    function _isBancorAddress(address _which) internal view returns (bool) {
+        address primaryAddress = address_belongs[_which];
+        uint256 flags = user_details[primaryAddress].flags;
+        return _checkRule(flags, BANCOR_ADDRESS, BANCOR_ADDRESS);
     }
 
     /**@dev checks if address is bypassed*/
-    function isAddressByPassed(address _which) public view returns (bool) {
+    function _isAddressByPassed(address _which) internal view returns (bool) {
         address primaryAddress = address_belongs[_which];
         uint256 flags = user_details[primaryAddress].flags;
         return _checkRule(flags, IS_BYPASSED, IS_BYPASSED);
     }
 
-    /**@dev checks if address is bancor's*/
-    function isBancorAddress(address _which) public view returns (bool) {
+    /**@dev checks if address is bypassed*/
+    function isAddressByPassed(address _which) internal view returns (bool) {
+        return _isAddressByPassed(_which);
+    }
+
+    /**@dev returns if address is whitelisted*/
+    function _isWhiteListed(address _which) internal view returns (bool) {
+        if (address_belongs[_which] == address(0)) return false;
+        return true;
+    }
+
+    /**@dev returns if address is whitelisted*/
+    function isWhiteListed(address _which) external view returns (bool) {
+        return _isWhiteListed(_which);
+    }
+
+    /**@dev checks if address is allowed in auction or not */
+    function isAllowedInAuction(address _which) external view returns (bool) {
         address primaryAddress = address_belongs[_which];
         uint256 flags = user_details[primaryAddress].flags;
-        return _checkRule(flags, BANCOR_ADDRESS, BANCOR_ADDRESS);
+        return _checkRule(flags, IS_ALLOWED_AUCTION, IS_ALLOWED_AUCTION);
+    }
+
+    /**@dev checks if _which is allowed with buyback or not */
+    function isAllowedBuyBack(address _which) external view returns (bool) {
+        address primaryAddress = address_belongs[_which];
+        uint256 flags = user_details[primaryAddress].flags;
+        return _checkRule(flags, IS_ALLOWED_BUYBACK, IS_ALLOWED_BUYBACK);
+    }
+
+    /**@dev checks if address is allowed in auction or not */
+    function checkRule(address _which, uint256 _condition)
+        external
+        view
+        returns (bool)
+    {
+        address primaryAddress = address_belongs[_which];
+        uint256 flags = user_details[primaryAddress].flags;
+        return _checkRule(flags, _condition, _condition);
     }
 
     /**@dev whitelists an address*/
@@ -175,7 +213,7 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         uint64 _flags,
         uint256 _maxWallets
     ) public onlySystem() notZeroAddress(_which) returns (bool) {
-        require(!isWhiteListed(_which), "ERR_ACTION_NOT_ALLOWED");
+        require(!_isWhiteListed(_which), "ERR_ACTION_NOT_ALLOWED");
         return whiteListAccount(_which, _flags, _maxWallets);
     }
 
@@ -185,7 +223,7 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         onlyOwner
         returns (bool)
     {
-        require(isWhiteListed(_which), ERR_AUTHORIZED_ADDRESS_ONLY);
+        require(_isWhiteListed(_which), ERR_AUTHORIZED_ADDRESS_ONLY);
         UserDetails storage details = user_details[_which];
         details.maxWallets = _maxWallets;
         return true;
@@ -207,7 +245,7 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         address primaryAddress = address_belongs[sender];
 
         require(
-            isWhiteListed(primaryAddress) && sender == primaryAddress,
+            _isWhiteListed(primaryAddress) && sender == primaryAddress,
             ERR_AUTHORIZED_ADDRESS_ONLY
         );
 
@@ -234,7 +272,7 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         notZeroAddress(_which)
         returns (bool)
     {
-        require(isWhiteListed(_which), "ERR_ACTION_NOT_ALLOWED");
+        require(_isWhiteListed(_which), "ERR_ACTION_NOT_ALLOWED");
         address primaryAddress = address_belongs[_which];
         user_details[primaryAddress].flags = _flags;
         emit FlagsChanged(_which, _flags);
@@ -323,23 +361,25 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         address to = address_belongs[_to];
         address msgSender = address_belongs[_msgSender];
         bool result;
+
         //If transfer is happening to a bypassed address then check nothing
-        if (isAddressByPassed(to)) {
+        if (_isAddressByPassed(to)) {
             return true;
         }
         //If a byPassed Address calls a transfer or transferFrom function then just check if _to is whitelisted and return
-        if (isAddressByPassed(msgSender)) {
-            result = isWhiteListed(to);
+        if (_isAddressByPassed(msgSender)) {
+            result = _isWhiteListed(to);
             return result;
         }
         //Added to make sure that bancor addresses transfer to bypassed addresses only
         //if a bancor address calls a transfer or transferFrom function then return true only if to is Bypassed a
-        
-        if (isBancorAddress(msgSender)){
-            if(isBancorAddress(to)) return true;
-            else if(isAddressByPassed(to)) return true;
+
+        if (_isBancorAddress(msgSender)) {
+            if (_isBancorAddress(to))
+                return _isBancorAddress(_from) || _isAddressByPassed(_from);
+            else if (_isAddressByPassed(to)) return true;
             else return false;
-        }
+        } else if (_isBancorAddress(to)) return false;
 
         result = _isReceiveAllowed(_to, token); // Check receiver at first
         if (!result) return false; // if receiver disallowed the transfer disallowed too.
@@ -524,10 +564,25 @@ contract WhiteList is Upgradeable, ProxyOwnable, SafeMath, WhiteListStorage,Init
         return details.wallets;
     }
 
-    /**@dev checks if _which is allowed with buyback or not */
-    function isAllowedBuyBack(address _which) public view returns (bool) {
-        address primaryAddress = address_belongs[_which];
-        uint256 flags = user_details[primaryAddress].flags;
-        return _checkRule(flags, IS_ALLOWED_BUYBACK, IS_ALLOWED_BUYBACK);
+    function updateHoldBackDays(uint8 _token, uint256 _holdBackDay)
+        public
+        onlyAuthorized()
+        returns (bool)
+    {
+        tokenToHoldBackDaysTimeStamp[_token] = convertDaysToTimeStamp(
+            _holdBackDay
+        );
+        return true;
+    }
+
+    function updateMaturityDays(uint8 _token, uint256 _maturityDays)
+        public
+        onlyAuthorized()
+        returns (bool)
+    {
+        tokenToMaturityDaysTimeStamp[_token] = convertDaysToTimeStamp(
+            _maturityDays
+        );
+        return true;
     }
 }
