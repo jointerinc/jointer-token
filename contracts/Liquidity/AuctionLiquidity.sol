aa8e2b92272e3cf6ac45ef7eb4c9bef2506cb830
Mukesh Makwana <mukesh.makwana@minddeft.com>
2020-09-21 17:44:10 +0530

    liquidity path removed

--- contracts/Liquidity/AuctionLiquidity.sol
+++ contracts/Liquidity/AuctionLiquidity.sol
@@ -577,28 +577,26 @@ contract Liquidity is
     }
 
     function redemptionFromEscrow(
-        address[] memory _path,
         uint256 _amount,
         address payable _reciver
     ) public returns (bool) {
         require(msg.sender == escrowAddress, ERR_AUTHORIZED_ADDRESS_ONLY);
-        return _redemption(_path, _amount, msg.sender, _reciver);
+        return _redemption(_amount, msg.sender, _reciver);
     }
 
-    function redemption(address[] memory _path, uint256 _amount)
+    function redemption(uint256 _amount)
         public
         returns (bool)
     {
-        return _redemption(_path, _amount, msg.sender, msg.sender);
+        return _redemption(_amount, msg.sender, msg.sender);
     }
 
     function _redemption(
-        address[] memory _path,
         uint256 _amount,
         address payable _caller,
         address payable _reciver
     ) internal returns (bool) {
-        require(_path[0] == mainToken, "ERR_MAIN_TOKEN");
+        
 
         address primaryWallet = IWhiteList(whiteListAddress).address_belongs(
             _reciver
@@ -633,7 +631,7 @@ contract Liquidity is
         uint256 _afterBalance = converter.balance;
 
         emit Redemption(
-            address(_path[safeSub(_path.length, 1)]),
+            baseToken,
             _amount,
             returnAmount
         );
